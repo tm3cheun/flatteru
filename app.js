2ae8d17f092da45cac5ed1658f5b5ac451118df1
Tiffany <tiffany.cheung23@gmail.com>
2017-09-25 22:17:57 -0400

    Fix the listening port

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ var db = mongoose.connect(process.env.MONGODB_URI);
 var compliments = require("./models/compliments");
 
 var app = express();
-app.set('port', (process.env.PORT || 5000))
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-// Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
 // Server index page
 app.get("/", function (req, res) {
-  res.send("Deployed!");
+	res.send("Deployed!");
 });
 
 // Facebook Webhook
